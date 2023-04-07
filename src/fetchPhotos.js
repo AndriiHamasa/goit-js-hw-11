29187fe7b2cc0d6c06e78c745cb78e2641787efb
AndriiHamasa <andrii.khamaza@nure.ua>
2023-04-07 10:26:22 +0300

    made all functional

--- src/fetchPhotos.js
+++ src/fetchPhotos.js
@@ -1,18 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "https://pixabay.com/api";
-const API_KEY = "35021864-8bcde3535af483a0723c672d5";
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '35021864-8bcde3535af483a0723c672d5';
 
-const searchParams = new URLSearchParams({
-    key: API_KEY,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: true
-})
+export const searchParams = {
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
+  page: 1,
+};
 
-export const fetchPhotos = async (str) => {
-    const {data} = await axios.get(`${BASE_URL}/?${searchParams}&q=${str}`);
-    console.log('data: ', data);
+export const fetchPhotos = async str => {
+  const { data } = await axios.get(
+    `${BASE_URL}/?${new URLSearchParams(searchParams)}&q=${str}`
+  );
 
-    return data;
-}
+  console.log('data: ', data);
+
+  return data;
+};
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,119 @@
-import Notiflix from "notiflix";
-import { fetchPhotos } from "./fetchPhotos";
+import Notiflix from 'notiflix';
+import { fetchPhotos, searchParams } from './fetchPhotos';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const inputEl = document.querySelector('.search-input');
-const btnEl = document.querySelector('button');
+const formEl = document.querySelector('.search-form');
+const inputEl = formEl.firstElementChild;
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtnEl = document.querySelector('.load-more');
 
-btnEl.addEventListener('click', async (event) => {
-    event.preventDefault();
+const renderMurkupForCard = array => {
+  const newArray = array.map(
+    card => `<div class="photo-card">
+    <a href="${card.largeImageURL}" class="photo-ancor"><img class="photo" src="${card.webformatURL}" alt="${card.tags}" loading="lazy" /></a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        <span>${card.likes}</span>
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        <span>${card.views}</span>
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        <span>${card.comments}</span>
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        <span>${card.downloads}</span>
+      </p>
+    </div>
+  </div>`
+  );
 
-    console.log('the START of all functions');
+  return newArray.join('');
+};
 
-    const data = await fetchPhotos(inputEl.value);
+const toRemoveLoadMoreBtn = data => {
+  if (searchParams.page >= Math.ceil(data.totalHits / searchParams.per_page)) {
+    loadMoreBtnEl.classList.remove('js-load-more');
+    if (data.totalHits < searchParams.per_page) {
+      return;
+    }
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+};
 
-    console.log('the END of all functions');
-})
+const toScroll = () => {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+formEl.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  try {
+    loadMoreBtnEl.classList.remove('js-load-more');
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (inputEl.value === '') {
+    return Notiflix.Notify.info('Wow, Enter some text.');
+  }
+
+  searchParams.page = 1;
+
+  const data = await fetchPhotos(inputEl.value);
+
+  if (data.hits.length === 0) {
+    return Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+  galleryEl.innerHTML = renderMurkupForCard(data.hits);
+  toScroll();
+  searchParams.page += 1;
+
+  // ============
+  let gallery = new SimpleLightbox('.gallery a');
+
+  gallery.on('show.simplelightbox', function () {});
+  // ============
+
+  loadMoreBtnEl.classList.add('js-load-more');
+
+  toRemoveLoadMoreBtn(data);
+});
+
+loadMoreBtnEl.addEventListener('click', async () => {
+  console.log('load more');
+  const data = await fetchPhotos(inputEl.value);
+  galleryEl.insertAdjacentHTML('beforeend', renderMurkupForCard(data.hits));
+  toScroll();
+
+  // ============
+  let gallery = new SimpleLightbox('.gallery a');
+
+  gallery.on('show.simplelightbox', function () {});
+  // ============
+
+  toRemoveLoadMoreBtn(data);
+
+  searchParams.page += 1;
+});
 
 
